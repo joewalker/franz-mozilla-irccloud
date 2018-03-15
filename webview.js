da16609528d366636a96da71a162e1a50ca0af94
Joe Walker <jwalker@mozilla.com>
2018-03-15 13:44:52 +0000

    Updates for Franz v5

--- webview.js
+++ webview.js
@@ -1,19 +1,13 @@
 
 const path = require('path');
 
-module.exports = (Franz, options) => {
+module.exports = Franz => {
   const getMessages = () => {
-    // Zero effort here. We should be able to get the count for the badge from
-    // irccloud, and probably the default impl should us how but I couldn't see
-    // the source, so this won't work
+    const directMessages = document.getElementsByClassName("buffer conversation active unread activeBadge").length;
+    const indirectMessages = document.getElementsByClassName("buffer channel active unread").length;
 
-    const updates = document.getElementById('franz').getAttribute('data-unread');
-    const inbox = document.getElementById('franz').getAttribute('data-inbox');
-    Franz.setBadge(updates, inbox);
+    Franz.setBadge(directMessages, indirectMessages);
   };
 
-  Franz.injectCSS(path.join(__dirname, 'franz.css'));
-
-  // called every 1s
   Franz.loop(getMessages);
 };
